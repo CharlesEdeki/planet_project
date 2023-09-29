9d43ff5c148f681c1fd0e4701a62408725be258f
CharlesEdeki <charlesedeki093@gmail.com>
2023-09-29 01:27:33 +0100

    Added a git ignore fileWorking with kepler dataset to find planets that are habitable

---
+++ index.js
@@ -0,0 +1,30 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const habitablePlanets = [];
+
+// function to check if the planet is habitable
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11 
+    && planet['koi_prad'] < 1.6;
+}
+
+// streaming the csv file and pushing the data to the results array
+fs.createReadStream('keplers_dataset.csv')
+    .pipe(parse({  // using the csv-parse package
+        comment: '#', // ignoring the comments
+        columns: true // using the first row as the header
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)){ // checking if the planet is habitable
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.log(err); // handling error
+    })
+    .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`); // logging the number of habitable planets
+    });
